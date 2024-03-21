eab2e6b8319f6d48eb94137ceb789e5205c6062b
NICOLAS DE SOUZA SANTOS <nicolas.ssantos15@senacsp.edu.br>
2024-03-21 16:54:14 -0300

    Feature

--- resources/views/cep.blade.php
+++ resources/views/cep.blade.php
@@ -4,4 +4,25 @@
 <input type="text" id="cep" name="cep">
 <button onclick="buscarCep()">Buscar CEP</button>
 
+<div id="endereco">
+
+<h2>cep: </h2>
+<h2>logradouro:</h2>
+<h2>complemento:</h2>
+<h2>bairro:</h2>
+<h2>localidade:</h2>
+<h2>uf:</h2>
+<h2>ibge:</h2>
+<h2>gia:</h2>
+<h2>ddd:</h2>
+<h2>siafi:</h2>
+</div>
+
+
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+
+<script>
+    function buscarCEP(){
+        var cep = document.getElementById('cep').value;
+    }
+</script>
